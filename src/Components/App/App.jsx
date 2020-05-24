0110ba74d955ae8ec1c2dfdb4d08a47f0fcb3e70
Ivan Berk <berkoivan@gmail.com>
2020-05-24 22:53:37 +0300

    initial goit-react-hw-03-image-finder

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+// import axios from "axios";
+// import { v4 as uuidv4 } from "uuid";
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
